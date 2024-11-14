8710bec1fa94fa8bda68e8ecdf7b38468322a89c
vhrlaptop2 <94510806+vhrlaptop2@users.noreply.github.com>
2024-11-13 16:41:51 -0800

    Update Subsystem.java

--- src/main/java/frc/robot/StateBasedRobot/Subsystem.java
+++ src/main/java/frc/robot/StateBasedRobot/Subsystem.java
@@ -1,9 +1,5 @@
 package frc.robot.StateBasedRobot;
 
 public class Subsystem {
-    
-    private int state = 0;
-    public final void SetState(int state) {
-        this.state = state;
-    }
+
 }
